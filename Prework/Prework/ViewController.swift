760279c27e89205e85da302406eb39850d89d8f0
yhazir2017 <hhtopcu@users.noreply.github.com>
2017-02-25 14:38:42 -0800

    code updated

--- Prework/Prework/ViewController.swift
+++ Prework/Prework/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercentages=[0.18,0.20,0.25]
+        let bill =  Double(billField.text!) ?? 0
+        let tip = bill*tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%0.2f", tip)
+        totalLabel.text = String(format: "$%0.2f", total)
+    }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
 }
 
