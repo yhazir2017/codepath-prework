6d1c2cada0a01e0902329893a00e533cb7d4a5cb
yhazir2017 <hhtopcu@users.noreply.github.com>
2017-02-27 07:04:31 -0800

    modified for default users data

--- Prework/Prework/ViewController.swift
+++ Prework/Prework/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, DataEnteredDelegate{
+class ViewController: UIViewController,DataEnteredDelegate{
     
     @IBOutlet weak var billField: UITextField!
     
@@ -18,28 +18,29 @@ class ViewController: UIViewController, DataEnteredDelegate{
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     
-    var tip1:float_t=0.18
-    var tip2:float_t=0.20
-    var tip3:float_t=0.25
+    var tip1:Int=18
+    var tip2:Int=20
+    var tip3:Int=25
     
-    
-    var tipPercentagesArray=[Float]()
-    
-
-    
+     var tipPercentagesArray=[0.18,0.20,0.25]
+     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //secondVC.delegate=self
-    
         // Read user settings Data
+        tip1 = defaults.integer(forKey:"tip1")
+        tip2 = defaults.integer(forKey:"tip2")
+        tip3 = defaults.integer(forKey:"tip3")
         
-        let defaults = UserDefaults.standard
-        tip1 = defaults.float(forKey:"tip1")
-        tip2 = defaults.float(forKey:"tip2")
-        tip3 = defaults.float(forKey:"tip3")
+        let segmentlabel1:String=String(tip1)+"%"
+        let segmentlabel2:String=String(tip2)+"%"
+        let segmentlabel3:String=String(tip3)+"%"
         
+        tipControl.setTitle(segmentlabel1, forSegmentAt: 0)
+        tipControl.setTitle(segmentlabel2, forSegmentAt: 1)
+        tipControl.setTitle(segmentlabel3, forSegmentAt: 2)
+        tipPercentagesArray=[Double(tip1)/100.0,Double(tip2)/100.0,Double(tip3)/100.0]
         
     }
 
@@ -50,10 +51,6 @@ class ViewController: UIViewController, DataEnteredDelegate{
 
     @IBAction func calculateTip(_ sender: AnyObject) {
         
-       let tipPercentagesArray=[tip1,tip2,tip3]
-    
-        //let tipPercentagesArray=[0.18,0.20,0.25]
-
         let bill =  Double(billField.text!) ?? 0
         let tip = bill*Double(tipPercentagesArray[tipControl.selectedSegmentIndex])
         let total = bill + tip
@@ -63,18 +60,29 @@ class ViewController: UIViewController, DataEnteredDelegate{
 
     @IBAction func onTap(_ sender: Any) {
         
-        let defaults = UserDefaults.standard
-        defaults.set(tip1, forKey:"tip1")
-        defaults.set(tip2, forKey:"tip2")
-        defaults.set(tip3, forKey:"tip3")
         
         view.endEditing(true)
     }
     
-    func userDidEnterInformation(data1: Float, data2: Float, data3: Float) {
+    func userDidEnterInformation(data1: Int!, data2: Int!, data3: Int!) {
+        
         tip1=data1
         tip2=data2
         tip3=data3
+        
+        tipPercentagesArray=[Double(data1)/100.0,Double(data2)/100.0,Double(data3)/100.0]
+      
+        let segmentlabel1:String=String(tip1)+"%"
+        let segmentlabel2:String=String(tip2)+"%"
+        let segmentlabel3:String=String(tip3)+"%"
+        
+        tipControl.setTitle(segmentlabel1, forSegmentAt: 0)
+        tipControl.setTitle(segmentlabel2, forSegmentAt: 1)
+        tipControl.setTitle(segmentlabel3, forSegmentAt: 2)
+        
+        defaults.set(tip1, forKey:"tip1")
+        defaults.set(tip2, forKey:"tip2")
+        defaults.set(tip3, forKey:"tip3")
         
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
