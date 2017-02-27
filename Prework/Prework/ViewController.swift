26caf0bceb745be1f1ea4a1a84247fd728000ec6
yhazir2017 <hhtopcu@users.noreply.github.com>
2017-02-26 16:32:22 -0800

    seetting

--- Prework/Prework/ViewController.swift
+++ Prework/Prework/ViewController.swift
@@ -8,9 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-
+class ViewController: UIViewController, DataEnteredDelegate{
+    
     @IBOutlet weak var billField: UITextField!
     
     @IBOutlet weak var tipLabel: UILabel!
@@ -19,9 +18,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     
+    var tip1:float_t=0.18
+    var tip2:float_t=0.20
+    var tip3:float_t=0.25
+    
+    
+    var tipPercentagesArray=[Float]()
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //secondVC.delegate=self
+    
+        // Read user settings Data
+        
+        let defaults = UserDefaults.standard
+        tip1 = defaults.float(forKey:"tip1")
+        tip2 = defaults.float(forKey:"tip2")
+        tip3 = defaults.float(forKey:"tip3")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,16 +49,42 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculateTip(_ sender: AnyObject) {
-        let tipPercentages=[0.18,0.20,0.25]
+        
+       let tipPercentagesArray=[tip1,tip2,tip3]
+    
+        //let tipPercentagesArray=[0.18,0.20,0.25]
+
         let bill =  Double(billField.text!) ?? 0
-        let tip = bill*tipPercentages[tipControl.selectedSegmentIndex]
+        let tip = bill*Double(tipPercentagesArray[tipControl.selectedSegmentIndex])
         let total = bill + tip
         tipLabel.text = String(format: "$%0.2f", tip)
         totalLabel.text = String(format: "$%0.2f", total)
     }
 
     @IBAction func onTap(_ sender: Any) {
+        
+        let defaults = UserDefaults.standard
+        defaults.set(tip1, forKey:"tip1")
+        defaults.set(tip2, forKey:"tip2")
+        defaults.set(tip3, forKey:"tip3")
+        
         view.endEditing(true)
+    }
+    
+    func userDidEnterInformation(data1: Float, data2: Float, data3: Float) {
+        tip1=data1
+        tip2=data2
+        tip3=data3
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       
+        if segue.identifier=="showSendingVC"{
+            let sendingVC:SettingsViewController = segue.destination as! SettingsViewController
+            sendingVC.delegate = self
+        }
+        
+
     }
 }
 
