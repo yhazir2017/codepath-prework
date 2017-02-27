45a42bb42605cc873e8d657ae9fb36ce9e521c35
yhazir2017 <hhtopcu@users.noreply.github.com>
2017-02-27 07:05:07 -0800

    modified for default users data

--- Prework/Prework/SettingsViewController.swift
+++ Prework/Prework/SettingsViewController.swift
@@ -34,16 +34,15 @@ class SettingsViewController: UIViewController {
     
     @IBAction func saveButtonTapped(_ sender: Any) {
         if (delegate != nil){
-        let tip1=Float(tip1Field.text!)
-        let tip2=Float(tip2Field.text!)
-        let tip3=Float(tip3Field.text!)
-        print("tip1 :\(tip1) tip2:\(tip2) tip3:\(tip3)")
-        delegate?.userDidEnterInformation(data1: tip1,data2: tip2,data3: tip3)
+        let tip1=Int(tip1Field.text!)
+        let tip2=Int(tip2Field.text!)
+        let tip3=Int(tip3Field.text!)
+        delegate?.userDidEnterInformation(data1:tip1,data2:tip2,data3:tip3)
         }
     }
 
 }
 
 protocol DataEnteredDelegate {
-    func userDidEnterInformation(data1: Float!,data2: Float!,data3: Float!)
+    func userDidEnterInformation(data1: Int!,data2: Int!,data3: Int!)
 }
